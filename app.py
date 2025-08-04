d6d90ecc73e189e8bb13122df87e1b20a724cf55
Mac <mac@MacBook-Air-Mac.local>
2025-08-04 20:56:26 +0200

    upgrade mikstury

--- app.py
+++ app.py
@@ -32,3 +32,14 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
+@app.route("/use-item", methods=["POST"])
+def use_item():
+    global player
+    if player.use_item():
+        result = f"{player.name} użył mikstury i odzyskał 30 HP!"
+    else:
+        result = f"{player.name} nie ma już mikstur!"
+    
+    enemy = Enemy()  # możesz też zachować poprzedniego w sesji
+    return render_template("battle.html", player=player, enemy=enemy, result=result)
